e8bf106a2beed2577e2089b13bc6ed6cfd926954
JaeHwan Kim <iwantbaobab@gmail.com>
2025-08-08 09:14:11 +0900

    Feat(#14): bastion host를 위한 provisioner script 추가

--- code/terraform/modules/eks/access_entries.tf
+++ code/terraform/modules/eks/access_entries.tf
@@ -3,6 +3,8 @@ resource "aws_eks_access_entry" "bastion_host" {
   cluster_name = aws_eks_cluster.eks_cluster.name
   principal_arn = var.bastion_host_role_arn
   type = "STANDARD"
+
+  depends_on = [aws_eks_cluster.eks_cluster]
 }
 
 resource "aws_eks_access_policy_association" "bastion_admin" {
--- code/terraform/modules/eks/kubeconfig.tf
+++ code/terraform/modules/eks/kubeconfig.tf
@@ -1,14 +1,14 @@
-# # Bastion host에서 kubeconfig 업데이트 (SSM 사용)
-# resource "terraform_data" "update_kubeconfig_on_bastion" {
-#   provisioner "local-exec" {
-#     command = <<-EOT
-#       aws ssm send-command \
-#         --instance-ids ${var.bastion_host_id} \
-#         --document-name "AWS-RunShellScript" \
-#         --parameters 'commands=["aws eks update-kubeconfig --region ${var.region} --name ${var.project_name}-${var.env}-eks"]' \
-#         --region ${var.region}
-#     EOT
-#   }
+# Bastion host에서 kubeconfig 업데이트 (SSM 사용)
+resource "terraform_data" "update_kubeconfig_on_bastion" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws ssm send-command \
+        --instance-ids ${var.bastion_host_id} \
+        --document-name "AWS-RunShellScript" \
+        --parameters 'commands=["aws eks update-kubeconfig --region ${var.region} --name ${var.project_name}-${var.env}-eks"]' \
+        --region ${var.region}
+    EOT
+  }
   
-#   depends_on = [aws_eks_cluster.eks_cluster]
-# }
+  depends_on = [aws_eks_access_policy_association.bastion_admin]
+}
