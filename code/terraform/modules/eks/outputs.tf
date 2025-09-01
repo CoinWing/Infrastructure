6ca16d7981855b3e820b3b89c815ab31e58056fc
juyeongkim <wkdwn0414@naver.com>
2025-09-01 15:53:01 +0900

    Feat: EKS 클러스터 output 추가

--- code/terraform/modules/eks/outputs.tf
+++ code/terraform/modules/eks/outputs.tf
@@ -1,3 +1,28 @@
 output "eks_worker_ng_lt_id" {
   value = aws_launch_template.eks_worker_ng_lt.id
 }
+
+output "cluster_endpoint" {
+  description = "EKS Cluter API Server Endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "EKS Cluter Certificate Data"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_name" {
+  description = "EKS Cluster Name"
+  value      = module.eks.cluster_name
+}
+
+output "oidc_provider_arn" {
+  description = "EKS OIDC provider ARN"
+  value       = module.eks.oidc_provider_arn
+}
+
+output "cluster_dualstack_oidc_issuer_url" {
+  description = "EKS OIDC issuer url"
+  value       = module.eks.cluster_dualstack_oidc_issuer_url
+}
