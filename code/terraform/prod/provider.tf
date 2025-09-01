ce1b89dd7222074bcb952904511b1ed9bc33cc46
juyeongkim <wkdwn0414@naver.com>
2025-09-01 15:52:40 +0900

    Feat: Velero 설치를 위한 Helm Provider 설정 추가

--- code/terraform/prod/provider.tf
+++ code/terraform/prod/provider.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    helm = { 
+      source  = "hashicorp/helm"
+      version = "~> 2.9"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
   }
   required_version = ">= 0.13"
 
@@ -15,4 +23,16 @@ terraform {
 
 provider "aws" {
   region = var.region
+}
+
+provider "helm" {
+  kubernetes  {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec  {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
 }
