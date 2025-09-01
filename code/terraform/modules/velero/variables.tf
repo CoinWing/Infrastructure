810eaf2fde4cfc128eac27d385b5c89ff1f80b29
juyeongkim <wkdwn0414@naver.com>
2025-09-01 15:53:55 +0900

    Feat: Velero 변수 추가

---
+++ code/terraform/modules/velero/variables.tf
@@ -0,0 +1,44 @@
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+}
+
+variable "cluster_ca_data" {
+  description = "EKS Cluster Certificate"
+  type        = string
+}
+
+variable "cluster_dualstack_oidc_issuer_url" {
+  description = "EKS OIDC issuer url"
+  type        = string
+}
+
+variable "oidc_provider_arn" {
+  description = "EKS OIDC provider ARN"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Velero bucket name"
+  type        = string
+}
+
+variable "namespace" {
+    description = "Velero namespace"
+    type        = string
+}
+
+variable "helm_chart_version" {
+  description = "Velero Helm Chart version"
+  type        = string
+}
+
+variable "velero_image_version" {
+  description = "Velero image version"
+  type        = string
+}
