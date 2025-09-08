1480007d0f890d5058fd7c3deba3a44ecb350fc5
juyeongkim <wkdwn0414@naver.com>
2025-09-08 21:29:41 +0900

    Fix: 권한 추가 및 plugin 버전 변수 추가 serviceaccount 이름 변경

--- code/terraform/modules/velero/velero.tf
+++ code/terraform/modules/velero/velero.tf
@@ -9,43 +9,22 @@ terraform {
   }
 }
 
-# S3 버킷 생성
-# Velero용 버킷
-resource "aws_s3_bucket" "velero" {
-  bucket = "${var.project_name}-velero-backup"
+data "aws_iam_policy_document" "velero_assume" {
+    statement {
+        actions = ["sts:AssumeRoleWithWebIdentity"]
+        effect  = "Allow"
 
-  tags = {
-    Name = "${var.project_name}-velero-backup"
-  }
-}
+        condition {
+          test     = "StringEquals"
+          variable = "${replace(var.cluster_oidc_issuer_url, "https://", "")}:sub"
+          values   = ["system:serviceaccount:${var.namespace}:velero-server"]
+        }
 
-# 퍼블릭 엑세스 차단
-resource "aws_s3_bucket_public_access_block" "velero" {
-  bucket = aws_s3_bucket.velero.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-# 서버사이드 암호화
-resource "aws_s3_bucket_server_side_encryption_configuration" "velero" {
-  bucket = aws_s3_bucket.velero.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+        principals {
+          type        = "Federated"
+          identifiers = [var.oidc_provider_arn]
+        }
     }
-  }
-}
-
-# 버전 관리 활성화
-resource "aws_s3_bucket_versioning" "velero" {
-  bucket = aws_s3_bucket.velero.id
-  versioning_configuration {
-    status = "Enabled"
-  }
 }
 
 # IAM 역할 생성
@@ -68,6 +47,8 @@ resource "aws_iam_policy" "velero" {
           "s3:DeleteObject",
           "s3:GetObject",
           "s3:PutObject",
+          "s3:PutObjectTagging",
+          "s3:ListMultipartUploadParts",
           "s3:ListBucket"
         ]
         Resource = [
@@ -82,7 +63,8 @@ resource "aws_iam_policy" "velero" {
           "ec2:DeleteSnapshot",
           "ec2:DescribeSnapshots",
           "ec2:DescribeVolumes",
-          "ec2:CreateTags"
+          "ec2:CreateTags",
+          "ec2:CreateVolume",
         ]
         Resource = "*"
       }
@@ -95,22 +77,43 @@ resource "aws_iam_role_policy_attachment" "velero" {
   policy_arn = aws_iam_policy.velero.arn
 }
 
-data "aws_iam_policy_document" "velero_assume" {
-    statement {
-        actions = ["sts:AssumeRoleWithWebIdentity"]
-        effect  = "Allow"
+# S3 버킷 생성
+# Velero용 버킷
+resource "aws_s3_bucket" "velero" {
+  bucket = "${var.project_name}-velero-backup"
 
-        condition {
-          test     = "StringEquals"
-          variable = "${replace(var.cluster_dualstack_oidc_issuer_url, "https://", "")}:sub"
-          values   = ["system:serviceaccount:${var.namespace}:velero"]
-        }
+  tags = {
+    Name = "${var.project_name}-velero-backup"
+  }
+}
 
-        principals {
-          type        = "Federated"
-          identifiers = [var.oidc_provider_arn]
-        }
+# 퍼블릭 엑세스 차단
+# resource "aws_s3_bucket_public_access_block" "velero" {
+#   bucket = aws_s3_bucket.velero.id
+# 
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
+
+# 서버사이드 암호화
+resource "aws_s3_bucket_server_side_encryption_configuration" "velero" {
+  bucket = aws_s3_bucket.velero.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
+}
+
+# 버전 관리 활성화
+resource "aws_s3_bucket_versioning" "velero" {
+  bucket = aws_s3_bucket.velero.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 # Velero Helm 차트 설치
@@ -124,17 +127,18 @@ resource "helm_release" "velero" {
 
   values = [
     templatefile("${path.module}/velero-values.yaml.tmpl", {
-      namespace            = var.namespace
-      bucket_name          = aws_s3_bucket.velero.bucket
-      region               = var.region
-      velero_role_arn      = aws_iam_role.velero.arn
-      velero_image_version = var.velero_image_version
+      namespace             = var.namespace
+      bucket_name           = aws_s3_bucket.velero.bucket
+      region                = var.region
+      velero_role_arn       = aws_iam_role.velero.arn
+      velero_image_version  = var.velero_image_version
+      velero_plugin_version = var.velero_plugin_version
     }),
     # 백업 스케줄 설정
     yamlencode({
       schedules = {
         "daliy-backup" = {
-          schedule        = "0 12 * * *"
+          schedule        = "0 17 * * *"
           template = {
             ttl              = "168h"
             includedNamespaces = ["*"]
