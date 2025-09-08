ec8995ece54dcbe962b5c2b8dd3b6481657b453c
juyeongkim <wkdwn0414@naver.com>
2025-09-08 21:24:51 +0900

    Fix: eks outputs 수정

--- code/terraform/modules/eks/outputs.tf
+++ code/terraform/modules/eks/outputs.tf
@@ -22,7 +22,7 @@ output "oidc_provider_arn" {
   value       = module.eks.oidc_provider_arn
 }
 
-output "cluster_dualstack_oidc_issuer_url" {
+output "cluster_oidc_issuer_url" {
   description = "EKS OIDC issuer url"
-  value       = module.eks.cluster_dualstack_oidc_issuer_url
+  value       = module.eks.cluster_oidc_issuer_url
 }
